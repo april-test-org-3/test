0139a8d6b52e519574f41d7d4223f4c158736616
aprilgill <139922752+aprilgill@users.noreply.github.com>
2024-11-25 19:20:15 -0800

    Create code.kt

---
+++ code.kt
@@ -0,0 +1,31 @@
+class Calculator {
+    fun addNumbers(a: Int, b: Int): Int {
+        return a + b
+    }
+    fun addNumbers2(a: Int, b: Int): Int {
+        return a + b
+    }
+    fun addNumbers3(a: Int, b: Int): Int {
+        return a + b
+    }
+    fun addNumbers4(a: Int, b: Int): Int {
+        return a + b
+    }
+    fun addNumbers5(a: Int, b: Int): Int {
+        return a + b
+    }
+    fun addNumbers6(a: Int, b: Int): Int {
+        return a + b
+    }
+    fun addNumbers7(a: Int, b: Int): Int {
+        return a + b
+    }
+    fun addNumbers8(a: Int, b: Int): Int {
+        return a + b
+    }
+}
+fun main() {
+    val calculator = Calculator()
+    val sum = calculator.addNumbers(5, 10)
+    println("The sum is: $sum")
+}
